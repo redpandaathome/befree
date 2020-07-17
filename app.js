9c9882bf9d8447fcd823bb8f197d6374ca37aa6f
redpandaathome <47564249+redpandaathome@users.noreply.github.com>
2020-07-17 15:15:36 +0900

    port change

--- app.js
+++ app.js
@@ -69,8 +69,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments" , commentRoutes);
 
-//var port = process.env.PORT || 3000;
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log("YelpCamp:v12 Has Started..!");
+  console.log("Server Has Started..!");
 });

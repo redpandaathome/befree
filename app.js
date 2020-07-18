77e6b495ea11aec6770a10fcaf374c3d6999bbbe
redpandaathome <47564249+redpandaathome@users.noreply.github.com>
2020-07-18 16:53:09 +0900

    changed db env variable

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
-//console.log(process.env.DATABASEURL);
-
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+
+//console.log(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v12"
+mongoose.connect(url, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
